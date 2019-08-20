7e89e4c163a3c5bf8fde2c85e401d27e0b0cc656
ShawLive <1610372224@qq.com>
2019-08-21 00:19:54 +0800

    state访问状态对象

--- jspang_vuex/src/components/Count.vue
+++ jspang_vuex/src/components/Count.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>{{msg}}</h2>
     <hr>
-    <h3>{{$store.state.count}}</h3>
+    <h3>{{$store.state.count}}={{count}}</h3>
     <div>
       <button @click="$store.commit('add')">+1</button>
       <button @click="$store.commit('reduce')">-1</button>
@@ -12,12 +12,33 @@
 
 <script>
 import store from '@/vuex/store'
+// 方法二：通过mapState的对象来赋值
+import {mapState} from 'vuex'
 export default {
   data () {
     return {
       msg: 'Hello Vuex'
     }
   },
-  store
+  store,
+  // 方法一：通过computed的计算属性直接赋值
+  // computed:{
+  // count () {
+  // 一定要写this，要不你会找不到$store的
+  //     return this.$store.state.count;
+  //   }
+  // }
+  // 方法二：通过mapState的对象来赋值
+  // computed:mapState({
+  //   // 使用ES6语法的箭头函数来给count赋值
+  //   // count:state=>state.count
+  //   // 使用ES5语法
+  //   count: function (state) {
+  //       return state.count
+  //   }
+  // })
+  // 方法三：通过mapState的数值来赋值
+  computed: mapState(['count'])
+
 }
 </script>
