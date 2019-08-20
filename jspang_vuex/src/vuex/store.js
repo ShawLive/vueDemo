3fb509a23a41a094b830901b3c6c7ca65ad3cd90
ShawLive <1610372224@qq.com>
2019-08-20 23:02:58 +0800

    jspang_vuex初始化小demo

---
+++ jspang_vuex/src/vuex/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'// 引入vue
+import Vuex from 'vuex'// 引入vuex
+
+// 使用vuex
+Vue.use(Vuex)
+
+// 声明一个常量对象state的count状态
+const state = {
+  count: 1
+}
+
+// mutations是固定的写法，意思是改变的
+const mutations = {
+  add (state) {
+    state.count++
+  },
+  reduce (state) {
+    state.count--
+  }
+}
+
+// 用export default封装代码，让外部可以引用
+export default new Vuex.Store({
+  state,
+  mutations
+})
