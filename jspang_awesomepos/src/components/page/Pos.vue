81cb4b7e8fa420cb729037dcaac9cca57839f321
ShawLive <1610372224@qq.com>
2019-09-08 15:18:31 +0800

    快餐店POS系统
    页面三栏式布局

---
+++ jspang_awesomepos/src/components/page/Pos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pos">
+    <el-row >
+      <el-col :span='7' class="pos-order" id="order-list">
+      我是订单栏
+      </el-col>
+      <!--商品展示-->
+      <el-col :span="17">
+        我是产品栏
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Pos',
+  mounted: function () {
+    var orderHeight = document.body.clientHeight
+    console.log(orderHeight)
+    document.getElementById('order-list').style.height = orderHeight + 'px'
+  }
+}
+</script>
+<style scoped>
+.pos-order {
+  background-color:#F9FAFC;
+  border-right: 1px solid #CeCCDA;
+}
+</style>
